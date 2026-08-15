e19d2dbea38fb4beac57417ed9b85daad7effe02
TBody8 <jugaralbaloncestomola@gmail.com>
2026-08-16 01:12:22 +0200

    fix(prod): add preview proxy and allowedHosts in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,20 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'https://sedeaplicaciones.minetur.gob.es',
+        changeOrigin: true,
+        rewrite: (path) =>
+          path.replace(/^\/api/, '/ServiciosRESTCarburantes/PreciosCarburantes'),
+      },
+    },
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'https://sedeaplicaciones.minetur.gob.es',
